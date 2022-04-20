109c5fb5810457bc835bbc6c70fc85e142ec0469
Vinny D <vdurham@prideseals.com>
2022-04-20 17:30:04 -0400

    can see and add notes

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const path = require("path");
 const fs = require('fs');
+const db = require('./db/db.json')
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static("public"));
+
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db/db.json', (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(data)
+        res.json(JSON.parse(data));
+    });
+});
+
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    db.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(db)
+    );
+    res.json(newNote);
+});
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
