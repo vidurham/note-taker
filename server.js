094dae11a4b49a43b0df0aea04d79c67501f162d
Vinny D <vdurham@prideseals.com>
2022-04-20 16:22:58 -0400

    html routes created

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require("path");
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,3 +9,15 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server now on port ${PORT}!`);
+});
